d4e6bd6ca8c1f853ec2dc1c57d5c1dd46720c402
jeruxniviz <jeruxniviz@gmail.com>
2022-01-23 19:09:18 +0700

    first commit

---
+++ resources/views/login.blade.php
@@ -0,0 +1,52 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title>Spp Kanesa | {{ $title }}</title>
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+
+
+    <!-- Custom styles for this template -->
+    <link href="css/signin.css" rel="stylesheet">
+  </head>
+  <body class="text-center container py-5 ">
+
+    <aside>
+        <img class="mb-4" src="img/login_image_1.png" alt="" >
+        <h1>APLIKASI PEMBAYARAN SPP</h1>
+        <h3>SMK NEGERI 1 KEPANJEN</h3>
+    </aside>
+
+    <main class="form-signin">
+    <form class="container ">
+        <img class="mb-4" src="img/kanesa_logo.png" alt="" width="100" height="100">
+        <h1 class="h3 mb-3 fw-normal">Silahkan Login</h1>
+
+        <div class="form-floating">
+        <input type="text" name="username" class="form-control" id="username" placeholder="Username">
+        <label for="username">Username</label>
+        </div>
+        <div class="form-floating">
+        <input type="password" name="password" class="form-control" id="password" placeholder="Password">
+        <label for="password">Password</label>
+        </div>
+
+
+        <div class="checkbox mb-3">
+        <label>
+            <input type="checkbox" value="remember-me"> Ingat saya
+        </label>
+        </div>
+        <button class="w-100 btn btn-lg btn-primary" type="submit">Masuk</button>
+        <p class="mt-5 mb-3 text-muted">&copy; Aziz-2022</p>
+    </form>
+    </main>
+  </body>
+</html>
